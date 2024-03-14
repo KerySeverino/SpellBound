cfc99e6c89eb40e6fff2b2bad3e6aeb6d0f45f09
Kery <Keryseverino@gmail.com>
2024-03-13 22:52:11 -0400

    Added Scrolling to the ImageLayer

--- src/ImageLayer.java
+++ src/ImageLayer.java
@@ -7,13 +7,31 @@ public class ImageLayer
 
 	public Image image;
 	
-	public ImageLayer(String filename) 
+	int x;
+	int y;
+	int z;
+	
+	public ImageLayer(String filename, int x, int y, int z) 
 	{
 		image = Toolkit.getDefaultToolkit().getImage(filename);
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public void moveLT(int dx)
+	{
+		x += dx / z;
+	}
+	
+	public void moveRT(int dy) {
+		x -= dy / z;
 	}
 	
 	public void draw(Graphics pen)
 	{
-		pen.drawImage(image, 0, 0, null);
+		for(int i = 0; i < 13; i++) {
+			pen.drawImage(image, x + i * 1800, y, 1800, 1000, null);	
+		}
 	}
 }
--- src/Spellbound.java
+++ src/Spellbound.java
@@ -6,18 +6,18 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 {	
 	//Background
 	//Image forest = Toolkit.getDefaultToolkit().getImage("forest_completed.png");
-	ImageLayer forest_11 = new ImageLayer("forest_11.png");
-	ImageLayer forest_10 = new ImageLayer("forest_10.png");
-	ImageLayer forest_9 = new ImageLayer("forest_9.png");
-	ImageLayer forest_8 = new ImageLayer("forest_8.png");
-	ImageLayer forest_7 = new ImageLayer("forest_7.png");
-	ImageLayer forest_6 = new ImageLayer("forest_6.png");
-	ImageLayer forest_5 = new ImageLayer("forest_5.png");
-	ImageLayer forest_4 = new ImageLayer("forest_4.png");
-	ImageLayer forest_3 = new ImageLayer("forest_3.png");
-	ImageLayer forest_2 = new ImageLayer("forest_2.png");
-	ImageLayer forest_1 = new ImageLayer("forest_1.png");
-	ImageLayer forest_0 = new ImageLayer("forest_0.png");
+	ImageLayer forest_11 = new ImageLayer("forest_11.png", 0, -270, 10);
+	ImageLayer forest_10 = new ImageLayer("forest_10.png", 0, -270, 10);
+	ImageLayer forest_9 = new ImageLayer("forest_9.png", 0, -270, 8);
+	ImageLayer forest_8 = new ImageLayer("forest_8.png", 0, -270, 8);
+	ImageLayer forest_7 = new ImageLayer("forest_7.png", 0, -270, 6);
+	ImageLayer forest_6 = new ImageLayer("forest_6.png", 0, -270, 6);
+	ImageLayer forest_5 = new ImageLayer("forest_5.png", 0, -270, 4);
+	ImageLayer forest_4 = new ImageLayer("forest_4.png", 0, -270, 4);
+	ImageLayer forest_3 = new ImageLayer("forest_3.png", 0, -270, 2);
+	ImageLayer forest_2 = new ImageLayer("forest_2.png", 0, -270, 2);
+	ImageLayer forest_1 = new ImageLayer("forest_1.png", 0, -270, 1);
+	ImageLayer forest_0 = new ImageLayer("forest_0.png", 0, -270, 1);
 
 	
 	
@@ -27,8 +27,8 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 	int [] venustrap_count = {4, 4, 6, 6, 6, 6};
 	int [] venustrap_duration = {10, 10, 10, 10, 6, 6};
 	
-	Hitbox venustrap_hitbox = new Hitbox(500, 716, 128, 128);
-	AI_control venustrap = new AI_control("venustrap", venustrap_pose, 500, 716, 128, 128, venustrap_count, venustrap_duration);
+	Hitbox venustrap_hitbox = new Hitbox(500, 520, 128, 128);
+	AI_control venustrap = new AI_control("venustrap", venustrap_pose, 500, 520, 128, 128, venustrap_count, venustrap_duration);
 	
 	//AI Enemy
 	String[] scorpion_pose = {"LTidle", "RTidle", "LTwalk", "RTwalk", "LTattack", "RTattack"};
@@ -36,7 +36,7 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 	int [] scorpion_duration = {10, 10, 10, 10, 6, 6, 15};
 		
 	Hitbox scorpion_hitbox = new Hitbox(300, 716, 48, 48);
-	AI_control scorpion = new AI_control("scorpion", scorpion_pose, 300, 798, 48, 48, scorpion_count, scorpion_duration);
+	AI_control scorpion = new AI_control("scorpion", scorpion_pose, 300, 602, 48, 48, scorpion_count, scorpion_duration);
 		
 	//PLAYER
 	String[] player_pose = {"LTidle", "RTidle", "LTwalk", "RTwalk", "LTrun", "RTrun", "LTdeath", "RTdeath"};
@@ -45,13 +45,13 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 			
 	
 	Hitbox player_hitbox = new Hitbox(50, 590, 50, 120);
-	Sprite player = new Sprite("wm", player_pose, 50, 590, 256, 256, player_count, player_duration);
+	Sprite player = new Sprite("wm", player_pose, 50, 395, 256, 256, player_count, player_duration);
 	Health_UI health = new Health_UI(20, 50,256,40);
 	
 	//Boundaries
-	Rect top_wall = new Rect(0, -50, 1500, 50);
+	Rect top_wall = new Rect(0, -50, 1800, 50);
 	Rect left_wall = new Rect(-50, 0, 50, 800);
-	Rect floor = new Rect(0, 845, 1500, 85);
+	Rect floor = new Rect(0, 650, 1800, 85);
 
 	//Controls
 	boolean UP_Pressed = false;
@@ -78,10 +78,34 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 			if(LT_Pressed && health.playerHealth > 0)
 			{
 				player.walkLT(2);
+				forest_11.moveLT(10);
+				forest_10.moveLT(10);
+				forest_9.moveLT(10);
+				forest_8.moveLT(10);
+				forest_7.moveLT(10);
+				forest_6.moveLT(10);
+				forest_5.moveLT(10);
+				forest_4.moveLT(10);
+				forest_3.moveLT(10);
+				forest_2.moveLT(10);
+				forest_1.moveLT(10);
+				forest_0.moveLT(10);
 			}
 			if(RT_Pressed && health.playerHealth > 0)
 			{
 				player.walkRT(2);
+				forest_11.moveRT(10);
+				forest_10.moveRT(10);
+				forest_9.moveRT(10);
+				forest_8.moveRT(10);
+				forest_7.moveRT(10);
+				forest_6.moveRT(10);
+				forest_5.moveRT(10);
+				forest_4.moveRT(10);
+				forest_3.moveRT(10);
+				forest_2.moveRT(10);
+				forest_1.moveRT(10);
+				forest_0.moveRT(10);
 			}
 			
 			//Running
@@ -101,7 +125,7 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 			scorpion_hitbox.track(scorpion);
 			//scorpion.evade(player_hitbox, 1);
 			
-			if(scorpion.x == 0 || scorpion.x == 1920) scorpion.x = 800;
+			if(scorpion.x == 0 || scorpion.x == 1800) scorpion.x = 800;
 			
 			//PLAYER
 			player_hitbox.player_track(player);
@@ -121,7 +145,7 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 	//Updates the image on the screen
 	public void update(Graphics pen)
 	{
-		offScreenPen.clearRect(0, 0, 1920, 1080);
+		offScreenPen.clearRect(0, 0, 1800, 1000);
 		
 		paint(offScreenPen);
 		
@@ -151,8 +175,8 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 	    health.draw(pen, player_hitbox, venustrap_hitbox);
 	    
 	    //	AI
-	    venustrap.ai_draw(pen, venustrap_hitbox, player_hitbox);
-	    scorpion.ai_draw(pen, scorpion_hitbox, player_hitbox);
+	    //venustrap.ai_draw(pen, venustrap_hitbox, player_hitbox);
+	    //scorpion.ai_draw(pen, scorpion_hitbox, player_hitbox);
 	    
 	    //Testing Tool
 	    if(testing_Tool == true) 
@@ -194,7 +218,7 @@ public class Spellbound extends Applet implements Runnable, KeyListener
 	
 	public void init()
 	{
-		offScreenImg = createImage(1920, 1080);
+		offScreenImg = createImage(1800, 1000);
 		offScreenPen = offScreenImg.getGraphics();
 		
 		addKeyListener(this);
